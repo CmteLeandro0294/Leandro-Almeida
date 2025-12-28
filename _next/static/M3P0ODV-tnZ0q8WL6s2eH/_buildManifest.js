500faa794c40b643921b41b90823420988cc74df
CmteLeandro0294 <224824195+CmteLeandro0294@users.noreply.github.com>
2025-12-28 12:46:21 -0500

    Deploy: GitHub Pages with themed illustrations

---
+++ _next/static/M3P0ODV-tnZ0q8WL6s2eH/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Leandro-Almeida//_next/:path+",
+        "destination": "/Leandro-Almeida/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
